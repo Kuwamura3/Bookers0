c94f82a0096a2e17f12dc9f99621d8a724934aed
EC2 Default User <ec2-user@ip-172-31-67-151.ec2.internal>
2020-09-16 00:27:52 +0000

    [Add] users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    @user = User.new
+    @users = User.page(params[:id]).reverse_order
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page]).reverse_order
