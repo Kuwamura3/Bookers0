a31a7d6bf58833f43a903e6e6217ced58239824b
EC2 Default User <ec2-user@ip-172-31-67-151.ec2.internal>
2020-09-17 07:10:11 +0000

    [Add] signin_with_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
